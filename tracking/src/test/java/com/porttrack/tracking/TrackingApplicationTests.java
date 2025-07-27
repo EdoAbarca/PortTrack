d9a71bdabbeea25951b2f625a6913a7c10025afe
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-07-27 16:11:06 -0400

    chore: Se inicializan microservicios
    Enunciado pide microservicios, el sistema no puede ser monolítico

---
+++ tracking/src/test/java/com/porttrack/tracking/TrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porttrack.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
